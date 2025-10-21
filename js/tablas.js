05f860a7130535633a23dc93f58766951e3e5571
Julián Aguilar <julian.aguilar0127@gmail.com>
2025-10-21 14:43:22 -0700

    Se agrego el archivo JSON con informacion de alumnos

--- js/tablas.js
+++ js/tablas.js
@@ -6,12 +6,12 @@ const tbody = document.getElementById("tablaBody");
 const btnAgregar = document.getElementById("btnAgregar");
 
 //funciones
-function agregarFila(){
+function agregarFila() {
     let txtId = id.value.trim();
     let txtNombre = nombre.value.trim();
     let txtEscuela = escuela.value.trim();
 
-    if(!txtId || !txtNombre || !txtEscuela){
+    if (!txtId || !txtNombre || !txtEscuela) {
         alert("Por favor completa los campos");
         return;
     }
@@ -32,7 +32,6 @@ function agregarFila(){
 
     //agregar la fila a la tabla
     tbody.appendChild(fila);
-    tbody.appendChild(tbody);
 
     //limpiar campos
     document.getElementById("idInput").value = "";
@@ -40,5 +39,37 @@ function agregarFila(){
     document.getElementById("escuelaInput").value = "";
 }
 
+function listarAlumnos() {
+    fetch('../js/alumnos.json')
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            const tabla = document.getElementById("tabla");
+            //ciclo
+            datos.forEach(alumno => {
+                //crear una nueva fila
+                const fila = document.createElement("tr");
+                // crear y agregar celdas
+                const celdaId = document.createElement("td")
+                celdaId.textContent = alumno.id;
+                fila.appendChild(celdaId);
+
+                const celdaNombre = document.createElement("td");
+                celdaNombre.textContent = alumno.nombre;
+                fila.appendChild(celdaNombre);
+
+                const celdaEscuela = document.createElement("td");
+                celdaEscuela.textContent = alumno.escuela;
+                fila.appendChild(celdaEscuela);
+
+                //agregar la fila a la tabla
+                tbody.appendChild(fila);
+
+            }); // termina el ciclo
+        }) // termina el ciclo
+        
+        .catch(error => console.log("Error: "+ console.error));
+}
+
 //eventos
-btnAgregar.addEventListener("click",agregarFila);
+btnAgregar.addEventListener("click", agregarFila);
+document.addEventListener("DOMContentLoaded",listarAlumnos);
